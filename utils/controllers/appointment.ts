10b225dd64990509684bd0228915968e8e2cd169
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-07-17 23:42:44 -0300

    add refactor and feature turn validation

--- utils/controllers/appointment.ts
+++ utils/controllers/appointment.ts
@@ -1,5 +1,5 @@
 import prisma from "@/app/libs/prismadb";
-import { Appointment } from "@prisma/client";
+import { Appointment, Day, Patient, Time } from "@prisma/client";
 
 
 export const getAppointments = async () => {
@@ -62,84 +62,143 @@ export const createAppointment = async (
   timeProp: TimeProp,
   appointmentId?: string,
 ) => {
-
-  if (appointmentId && appointmentId !== undefined && appointmentId !== "") {
-    if (appointmentId === "" || patientId === "" || dayId === "" || timeProp.timeId == "") {
-      throw new Error("Datos no validos")
-    }
+  if (!isValidFormData(patientId, dayId, timeProp)) {
+    throw new Error("Datos no válidos");
   }
 
-  if (patientId === "" || dayId === "" || timeProp.timeId == "") {
-    throw new Error("Datos no validos")
-  }
-
-  if (!patientId || !dayId || !timeProp || !timeProp.startTime || !timeProp.endTime || !timeProp.timeId) {
-    throw new Error("El formulario no es valido");
-  }
-
-
-  const day = await prisma.day.findUnique({
-    where: {
-      id: dayId,
-    },
-  });
-
-  let appointment
-
-  const patient = await prisma.patient.findUnique({
-    where: {
-      id: patientId,
-    },
-  });
-
-  if (appointmentId)
-    appointment = await prisma.appointment.findUniqueOrThrow({
-      where: {
-        id: appointmentId,
-      },
-    });
-
-  const time = await prisma.time.findUnique({
-    where: {
-      id: timeProp.timeId
-    }
-  })
-
+  const day = await findDay(dayId);
+  const patient = await findPatient(patientId);
+  const time = await findTime(timeProp.timeId);
 
   if (!patient) {
     throw new Error("El paciente no fue encontrado");
   }
 
   if (!day) {
-    throw new Error("El dia no fue encontrado");
+    throw new Error("El día no fue encontrado");
   }
 
   if (!time) {
     throw new Error("El tiempo no fue encontrado");
   }
 
-  if (appointment && time) {
-    return await prisma.appointment.update({
-      where: {
-        id: appointment.id,
-      },
-      data: {
-        busy: busy,
-        patientId: patient.id,
-        dayId: day.id,
-        timeId: time.id
-      },
-    });
+  const existingAppointment = await findAppointmentByDayAndTime(dayId, timeProp.timeId);
+  if (existingAppointment) {
+    throw new Error("Ya existe un turno en el mismo día y horario");
   }
 
+  if (appointmentId) {
+    const appointment = await findAppointment(appointmentId);
+    if (appointment && time) {
+      const isTimeOccupied = await isTimeOccupiedByOtherAppointment(appointmentId, dayId, timeProp.timeId);
+      if (isTimeOccupied) {
+        throw new Error("El horario ya está ocupado por otro turno");
+      }
+      return await updateAppointment(appointment, busy, patient, day, time);
+    }
+  }
+
+  return await createNewAppointment(day, busy, patient, timeProp.timeId);
+};
+
+const isValidFormData = (
+  patientId: string,
+  dayId: string,
+  timeProp: TimeProp
+) => {
+  return (
+    patientId !== "" &&
+    dayId !== "" &&
+    timeProp.timeId !== "" &&
+    timeProp.startTime !== "" &&
+    timeProp.endTime !== ""
+  );
+};
+
+const findDay = async (dayId: string) => {
+  return await prisma.day.findUnique({
+    where: {
+      id: dayId,
+    },
+  });
+};
+
+const findPatient = async (patientId: string) => {
+  return await prisma.patient.findUnique({
+    where: {
+      id: patientId,
+    },
+  });
+};
+
+const findAppointment = async (appointmentId: string) => {
+  return await prisma.appointment.findUniqueOrThrow({
+    where: {
+      id: appointmentId,
+    },
+  });
+};
+
+const findTime = async (timeId: string) => {
+  return await prisma.time.findUnique({
+    where: {
+      id: timeId,
+    },
+  });
+};
+
+const findAppointmentByDayAndTime = async (dayId: string, timeId: string) => {
+  return await prisma.appointment.findFirst({
+    where: {
+      dayId: dayId,
+      timeId: timeId,
+    },
+  });
+};
+
+const isTimeOccupiedByOtherAppointment = async (appointmentId: string, dayId: string, timeId: string) => {
+  return await prisma.appointment.findFirst({
+    where: {
+      id: { not: appointmentId },
+      dayId: dayId,
+      timeId: timeId,
+    },
+  });
+};
+
+const updateAppointment = async (
+  appointment: Appointment,
+  busy: boolean,
+  patient: Patient,
+  day: Day,
+  time: Time
+) => {
+  return await prisma.appointment.update({
+    where: {
+      id: appointment.id,
+    },
+    data: {
+      busy: busy,
+      patientId: patient.id,
+      dayId: day.id,
+      timeId: time.id,
+    },
+  });
+};
+
+const createNewAppointment = async (
+  day: Day,
+  busy: boolean,
+  patient: Patient,
+  timeId: string
+) => {
   return await prisma.appointment.create({
     data: {
       dayId: day.id,
       busy: busy,
       patientId: patient.id,
-      timeId: timeProp.timeId,
+      timeId: timeId,
     },
   });
-
 };
 
